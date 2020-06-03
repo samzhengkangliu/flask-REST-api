c843ac5e0a4dc17fc2336b04408a8391ad75e1ad
Maskz1 <o4d0b@outlook.com>
2020-06-03 00:15:13 -0700

    Fixed an error that caused 500 error

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ api.add_resource(Store, '/stores/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
+db.init_app(app)
+
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
